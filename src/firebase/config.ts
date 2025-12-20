652c3ab963eb1ece58e0791d40f29abeec8421cc
Carl <carlisat19@gmail.com>
2025-12-20 09:23:03 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,8 @@
-// This file is intentionally left blank.
-// The firebase config will be populated by the Firebase Studio backend.
 export const firebaseConfig = {
+  "projectId": "studio-9165057693-5c1af",
+  "appId": "1:320905706337:web:48f0b0f200b6334cb0a320",
   "apiKey": "AIzaSyB2coUWHxMY7AklxwyyGi5lgwkl3vLLiYU",
   "authDomain": "studio-9165057693-5c1af.firebaseapp.com",
-  "projectId": "studio-9165057693-5c1af",
-  "storageBucket": "studio-9165057693-5c1af.appspot.com",
-  "messagingSenderId": "320905706337",
-  "appId": "1:320905706337:web:48f0b0f200b6334cb0a320"
+  "measurementId": "",
+  "messagingSenderId": "320905706337"
 };
