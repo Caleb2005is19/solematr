a6800853e0a110edd0eb0ef7b5b34e5488a36067
Carl <carlisat19@gmail.com>
2025-12-20 09:20:12 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 // This file is intentionally left blank.
 // The firebase config will be populated by the Firebase Studio backend.
 export const firebaseConfig = {
-  "apiKey": "YOUR_API_KEY",
-  "authDomain": "your-project-id.firebaseapp.com",
-  "projectId": "your-project-id",
-  "storageBucket": "your-project-id.appspot.com",
-  "messagingSenderId": "YOUR_SENDER_ID",
-  "appId": "YOUR_APP_ID"
+  "apiKey": "AIzaSyB2coUWHxMY7AklxwyyGi5lgwkl3vLLiYU",
+  "authDomain": "studio-9165057693-5c1af.firebaseapp.com",
+  "projectId": "studio-9165057693-5c1af",
+  "storageBucket": "studio-9165057693-5c1af.appspot.com",
+  "messagingSenderId": "320905706337",
+  "appId": "1:320905706337:web:48f0b0f200b6334cb0a320"
 };
